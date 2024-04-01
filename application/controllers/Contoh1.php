8ac3dbc1368d195df530400dcd1198bfd027842a
AgungDwiPrastya <Agungdwiprastya331@gmail.com>
2024-04-01 20:48:46 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Agung Dwi Prastya
+        Saya tinggal dibekasi kegiatan yang saya suka traveling"; 
+}
+}
